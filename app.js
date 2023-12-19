e26acafae8164838fab27d34558f0fd70ccda1e5
bens8235 <bmshreeve3@gmail.com>
2023-12-19 23:30:22 +0000

    changes to not let user press numbers out of order

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let count = 1;
 let counter = 0;
 const click = document.getElementById("click");
 const circle = document.getElementById("circles");
+let previousCircle = 0;
 
 function numberOfPlayers() {
   circlesArrSlice = circlesArr.slice(0, numOfPlayers);
@@ -22,25 +23,31 @@ function randomNumber() {
 window.addEventListener("keydown", function (event) {
   if (firstPlayerPicked === false) {
     for (let i = 0; i < circlesArrSlice.length; i++) {
-      if (event.key === (i + 1).toString() && event.repeat === false) {
+      if (
+        event.key === (i + 1).toString() &&
+        event.repeat === false &&
+        Number(event.key) === previousCircle + 1
+      ) {
+        previousCircle += 1;
         circlesArrSlice[i].style.visibility = "visible";
-      }
-      if (event.key === circlesArrSlice.length.toString()) {
-        setTimeout(function () {
-          if (count === 1) {
-            randomNumber();
-            count += 1;
-            winner.innerHTML = `The winner is player ${randNum}`;
-            firstPlayerPicked = true;
-            for (let i = 0; i < circlesArrSlice.length; i++) {
-              if (randNum - 1 !== i) {
-                circlesArrSlice[i].style.visibility = "hidden";
-              } else {
-                circle.classList.add("wiggle");
+
+        if (event.key === circlesArrSlice.length.toString()) {
+          setTimeout(function () {
+            if (count === 1) {
+              randomNumber();
+              count += 1;
+              winner.innerHTML = `The winner is player ${randNum}`;
+              firstPlayerPicked = true;
+              for (let i = 0; i < circlesArrSlice.length; i++) {
+                if (randNum - 1 !== i) {
+                  circlesArrSlice[i].style.visibility = "hidden";
+                } else {
+                  circle.classList.add("wiggle");
+                }
               }
             }
-          }
-        }, 1000);
+          }, 1000);
+        }
       }
     }
   }
@@ -85,6 +92,7 @@ for (let i = 0; i < players.length; i++) {
       firstPlayerPicked = false;
       count = 1;
       counter = 0;
+      previousCircle = 0;
     }
     numOfPlayers = i + 2;
     numberOfPlayers();
